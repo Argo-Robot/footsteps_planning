ca4c3815cb8e4690c3dd21920ff272a6a1e0dd89
leo-berte <leonardo.bertelli@mail.polimi.it>
2025-01-23 11:53:01 +0100

    code cleaning for tutorial

--- README.md
+++ README.md
@@ -4,14 +4,39 @@
 
 ### 1) Global Trajectory
 
+![Argo](./images/global_traj.png)
+*Global Trajectory*
+
 ### 2) 2D Footsteps Planning
 
-### 3) 3D Footsteps Planning
+![Argo](./images/2D_footsteps.png)
+*2D Footsteps Planning*
 
-### 4) CoM Trajecotry Generation
+### 3) 3D Feet Trajectory Planning
 
-### 5) ZMP Trajectory Generation
+![Argo](./images/3D.png)
+*3D Feet Trajectory Planning*
+
+### 4) ZMP Trajectory Generation
+
+![Argo](./images/zmp.png)
+*ZMP Trajectory Generation*
+
+### 5) CoM Pelvis Trajectory Generation
+
+#### 5.1) Dynamics Relations with ZMP
+
+![Argo](./images/com_dyn.png)
+*CoM Trajectory Generation*
+
+#### 5.2) Open-Loop Sine Wave 
+
+![Argo](./images/com_sine.png)
+*CoM Trajectory Generation*
 
 ## Quadrupeds
 
-### 3D foot trajectory
+### 3D Feet Trajectory
+
+![Argo](./images/trot_gait.gif)
+*3D Feet Trajectory Planning for Trot Gait: Rear Right and Front Right Feet*
--- quadrupeds/quadrupeds_gait_generation.m
+++ quadrupeds/quadrupeds_gait_generation.m
@@ -1,18 +1,23 @@
+%% Generate an elliptical foot trajectory in 2D (xz plane) for a quadruped doing Trot Gait.
+
+%% The front left leg and the rear right leg move together.
+%% The front right leg and the rear left leg move together.
+
 % Parameters for the elliptical foot trajectory
 A_x = 0.1; % Amplitude in the forward (x) direction [meters]
 A_z = 0.05; % Amplitude in the vertical (z) direction [meters]
 
 % Define phase offsets for trot gait
-phi_rear_left_0 = 0;          % Rear left leg initial phase
+phi_rear_rigth_0 = 0;         % Rear right leg initial phase
 phi_front_right_0 = pi;       % Front right leg initial phase (opposite phase)
 
 % Define stepping frequency
-T0 = 3.0 # Gait cycle [s]
+T0 = 3.0; # Gait cycle [s]
 f0 = 1/T0; # Frequency [Hz] 
 
-% Distance between diagonal legs in the horizontal plane
-offset_x = 0.2; % Forward/backward offset [meters]
-offset_y = 0.0; % Lateral offset [meters]
+% Distance between diagonal legs in the x plane
+offset_x = 0.2; % [meters]
+offset_y = 0.0; % [meters]
 
 num_steps = 100; % Number of steps in the trajectory
 time = linspace(0, T0, num_steps);
@@ -24,17 +29,17 @@ phase = @(phi_0, t) mod(phi_0 + 2 * pi * f0 * t, 2 * pi);
 foot_trajectory = @(phi) [A_x * cos(phi); -A_z * sin(phi)]; 
 
 % Initialize storage for trajectories
-rear_left_traj = zeros(2, num_steps);
+rear_rigth_traj = zeros(2, num_steps);
 front_right_traj = zeros(2, num_steps);
 
 % Compute trajectories for each leg
 for i = 1:num_steps
     % Compute phase for each leg
-    phi_rl = phase(phi_rear_left_0, time(i));
+    phi_rr = phase(phi_rear_rigth_0, time(i));
     phi_fr = phase(phi_front_right_0, time(i));
     
     % Compute foot trajectories
-    rear_left_traj(:, i) = foot_trajectory(phi_rl) + [-offset_x; offset_y];
+    rear_rigth_traj(:, i) = foot_trajectory(phi_rr) + [-offset_x; offset_y];
     front_right_traj(:, i) = foot_trajectory(phi_fr) + [offset_x; offset_y];
 end
 
@@ -44,19 +49,24 @@ hold on;
 grid on;
 axis equal;
 xlim([-0.4, 0.4]);
-ylim([-0.2, 0.2]);
-xlabel('Forward direction (H_x) [m]');
-ylabel('Vertical direction (H_z) [m]');
-title('Foot Trajectory Animation (Trot Gait)');
-rear_left_plot = plot(0, 0, 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
+ylim([-0.3, 0.3]);
+xlabel('Forward X-direction [m]');
+ylabel('Vertical Z-direction [m]');
+title('Foot Trajectory Animation (Trot Gait)', 'FontSize', 15 );
+
+% Define plots for the legs
+rear_right_plot = plot(0, 0, 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g');
 front_right_plot = plot(0, 0, 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r');
-trajectory_line_rl = plot(rear_left_traj(1, :), rear_left_traj(2, :), 'g--');
+trajectory_line_rl = plot(rear_rigth_traj(1, :), rear_rigth_traj(2, :), 'g--');
 trajectory_line_fr = plot(front_right_traj(1, :), front_right_traj(2, :), 'r--');
+
+% Add legend
+legend([rear_right_plot, front_right_plot], 'Rear Right Leg', 'Front Right Leg', 'Location', 'best');
 
 % Animate foot trajectories
 for i = 1:num_steps
     % Update positions
-    set(rear_left_plot, 'XData', rear_left_traj(1, i), 'YData', rear_left_traj(2, i));
+    set(rear_right_plot, 'XData', rear_rigth_traj(1, i), 'YData', rear_rigth_traj(2, i));
     set(front_right_plot, 'XData', front_right_traj(1, i), 'YData', front_right_traj(2, i));
     
     % Pause to simulate real-time motion
@@ -64,3 +74,4 @@ for i = 1:num_steps
 end
 
 hold off;
+
